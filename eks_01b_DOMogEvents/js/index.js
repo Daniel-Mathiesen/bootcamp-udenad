c051920212e9b0b1c8d9c6994d6a955c3b693adc
Daniel-Mathiesen <6249585@rts-365.dk>
2021-08-17 14:35:28 +0200

    Add asignment 01b

--- eks_01b_DOMogEvents/js/index.js
+++ eks_01b_DOMogEvents/js/index.js
@@ -1,6 +1,35 @@
 // "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
+// "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
+var count = document.getElementById("text-count");
+var number = 5;
+var max = 10;
+var min = 0;
+var Increase = document.getElementById("btn-count-increase");
+var Decrease = document.getElementById("btn-count-decrease");
 document.addEventListener("DOMContentLoaded", () => {
 
-	// skriv din kode her...
+Increase.addEventListener("click", increase);
+
+function UpdateNumber(){
+	document.getElementById("text-count").innerHTML = number;
+};
+
+UpdateNumber();
+
+function increase(){
+	if(number<max){
+	number++;
+	UpdateNumber();
+	}
+};
+
+Decrease.addEventListener("click", decrease);
+
+function decrease(){
+	if (number>min) {
+		number--;
+		UpdateNumber();
+	}
+} 
 
 }); // Afslutter: DOMContentLoaded
