a60d33c5dffd0681b7d2bcc98a334c6ca5db2599
Daniel-Mathiesen <6249585@rts-365.dk>
2021-08-19 10:06:26 +0200

    Add solution to 01c

--- eks_01c_DOMogEvents/js/index.js
+++ eks_01c_DOMogEvents/js/index.js
@@ -1,6 +1,29 @@
 // "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
+var count = document.getElementById("text-count");
+var number = 5;
+var max = 10;
+var min = 0;
+var Increase = document.getElementById("btn-count-increase");
+var Decrease = document.getElementById("btn-count-decrease");
 document.addEventListener("DOMContentLoaded", () => {
 
-	// skriv din kode her...
+function UpdateNumber(){
+	document.getElementById("text-count").innerHTML = number;
+};
+
+UpdateNumber();
+
+function MaxMin(direction){
+	if(direction === "inc" && number<max){
+		number++;
+		UpdateNumber();
+	} else if(direction === "dec"&& number>min){
+		number--;
+		UpdateNumber();
+	}
+};
+
+Increase.addEventListener("click", () => MaxMin("inc"));
+Decrease.addEventListener("click", () => MaxMin("dec"));
 
 }); // Afslutter: DOMContentLoaded
