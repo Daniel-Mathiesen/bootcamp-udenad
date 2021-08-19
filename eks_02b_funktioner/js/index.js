ef0c8cdf3c56e29eeccad1df3bc7f48ef3c711a1
Daniel-Mathiesen <6249585@rts-365.dk>
2021-08-19 11:28:03 +0200

    add solution for 02a and 02b

--- eks_02b_funktioner/js/index.js
+++ eks_02b_funktioner/js/index.js
@@ -1,6 +1,12 @@
 // "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
+var text = " hvad hedder du?"
 document.addEventListener("DOMContentLoaded", () => {
 
-	// skriv din kode her...
+	function hils(navn){
+		console.log(`Hej med dig, ${navn}!`);
+	}
 
+	hils("Daniel");
+	hils("Caia");
+	hils("Lakrids :3"); 
 }); // Afslutter: DOMContentLoaded
